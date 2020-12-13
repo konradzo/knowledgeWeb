351a961484ee00ecd56499391961e5205fa14d89
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-12-13 19:50:43 +0100

    router added, category list component in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {CategoryListComponent} from './components/category-list/category-list.component';
+import {HttpClientModule} from '@angular/common/http';
+
+const routes: Routes = [
+  {path: 'categories', component: CategoryListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
